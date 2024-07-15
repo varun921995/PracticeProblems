cf9633fc9df15eae53d9e660233b94aba0371de6
Varun Mahagaokar <varun090295@gmail.com>
2024-07-15 12:07:05 -0400

    optimized code

--- topKElements/src/Main.java
+++ topKElements/src/Main.java
@@ -1,26 +1,38 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] nums = new int[] { 1, 1, 1, 2, 2, 3 };
         int k = 2;
-        topKFrequent(nums, k);
+        int[] result = topKFrequent(nums, k);
+        System.out.println(result.toString());
     }
 
     public static int[] topKFrequent(int[] nums, int k) {
+        int[] result = new int[k];
         Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+        Map<Integer, List<Integer>> frequencycountMap = new TreeMap<Integer, List<Integer>>(Collections.reverseOrder());
         for (int i : nums) {
             frequencyMap.put(i, frequencyMap.getOrDefault(i, 0) + 1);
         }
-
-        Map<Integer, Integer> topK = frequencyMap.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .limit(k)
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-        return topK.keySet().stream().mapToInt(i -> i).toArray();
-
+        for (int i : frequencyMap.keySet()) {
+            if (frequencycountMap.containsKey(frequencyMap.get(i))) {
+                frequencycountMap.get(frequencyMap.get(i)).add(i);
+            } else {
+                frequencycountMap.put(frequencyMap.get(i), new ArrayList<Integer>());
+                frequencycountMap.get(frequencyMap.get(i)).add(i);
+            }
+        }
+        int resultLength = 0;
+        for (int i : frequencycountMap.keySet()) {
+            for (int j = 0; j < frequencycountMap.get(i).size(); j++) {
+                if (resultLength == k) {
+                    return result;
+                }
+                result[resultLength++] = frequencycountMap.get(i).get(j);
+            }
+        }
+        return result;
     }
 }
