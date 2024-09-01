2ff61a52a626ebc3a597b0200d99abce2f00ef2e
Varun Mahagaokar <varun090295@gmail.com>
2024-09-01 04:02:26 -0400

    Hacker Rank - New Year Chaos

---
+++ NewYearChaos/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> q = Arrays.asList(1,2,5,3,7,8,6,4);
+        minimumBribes(q);
+    }
+
+    public static void minimumBribes(List<Integer> q) {
+        // Write your code here
+       int bribe =  0 ;
+        for (int i = q.size() -1; i >=0 ; i--) {
+            if(q.get(i) != i+1){
+                if(q.get(i-1) == i+1){
+                    bribe += 1;
+                    int temp = q.get(i-1);
+                    q.set(i-1,q.get(i));
+                    q.set(i, temp);
+                }else if(q.get(i-2) == i+1){
+                    bribe += 2;
+                    int temp = q.get(i-2);
+                    q.set(i-2, q.get(i-1));
+                    q.set(i-1, q.get(i));
+                    q.set(i, temp);
+                }else{
+                    System.out.println("Too chaotic");
+                    return;
+                }
+            }
+        }
+        System.out.println(bribe);
+    }
+}
